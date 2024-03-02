902631d44dc456e4ded206c05b32cce0a990bb08
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2024-03-02 14:55:28 -0600

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
-import {MainView} from './components/Mainview/MainView';
+import {MainView} from './components/mainview/MainView';
+import {LoginView} from './components/login-view/login-view';
+import { SignupView } from './components/signup-view/signup-view';
+
+const router  = createBrowserRouter([
+  {
+    path: '/movies',
+    element: <MoviesView />,
+    errorElement: <NoFoundPage />
+  },
+  {
+    path: '/profile',
+    element: <ProfileView />,
+    errorElement: <NoFoundPage />
+  },
+  {
+    path: '/login',
+    element: <LoginView />,
+    errorElement: <NoFoundPage />
+  },
+  {
+    path: '/signup',
+    element: <SignupView />,
+    errorElement: <NoFoundPage />
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
         < MainView />
-      
+      </Container>
     </div>
   );
 }
