a527d0d6793d7a920957ff8af5bbe0d575bc0f6e
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2024-04-04 21:11:44 -0500

    Not Found Page fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LoginView } from './components/login-view/login-view';
 import { MovieCard } from './components/movieCard/MovieCard';
 import { UpdateProfileForm } from './components/Profile/updateUser';
 import { ProfileView } from './components/Profile/profile-view';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import { NotFoundPage } from './components/notFound/NoFoundPage';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <LoginView />,
-    },
-    {
-      path: '/profile',
-      element: <ProfileView />,
-    },
-    {
-      path: '/home',
-      element: <MovieCard />,
-    },
-    {
-      path: '/updateProfile',
-      element: <UpdateProfileForm />,
-    },
-  ]);
-
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginView />} />
+          <Route path="/profile" element={<ProfileView />} />
+          <Route path="/home" element={<MovieCard />} />
+          <Route path="/updateProfile" element={<UpdateProfileForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
--- src/components/notFound/NoFoundPage.jsx
+++ src/components/notFound/NoFoundPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-export function NotFoundPage(errorElement) {
+export function NotFoundPage() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1); // Go back to the previous page
+  };
+
   return (
     <div className="flex flex-col gap-2">
-      404 Not Found
-      <Link to="/login">Login page</Link>
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <button onClick={goBack}>Go back</button>
     </div>
   );
 }
